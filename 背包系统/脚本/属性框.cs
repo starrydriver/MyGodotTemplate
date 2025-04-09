d6a1283a76ef0cd4b4687a1c132816f8d223cac7
starrydriver <150936096+starrydriver@users.noreply.github.com>
2025-04-09 21:44:59 +0800

    展示补充

---
+++ 背包系统/脚本/属性框.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class 属性框 : Panel
+{
+	[Export] public string 名称 = "null";
+	[Export] public string 简介 = "null";
+	public override void _Ready()
+	{
+	}
+	public override void _Process(double delta)
+	{
+	}
+}
